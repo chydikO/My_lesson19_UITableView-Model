55d9b283d7178d8c58545b640cc7b7586216f661
chydikO <oleg.chudnovskiy@gmail.com>
2020-05-10 20:14:26 +0300

    empty project

---
+++ My_lesson19_UITableView+Model/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My_lesson19_UITableView+Model
+//
+//  Created by Олег Чудновский on 10.05.2020.
+//  Copyright © 2020 Олег Чудновский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
