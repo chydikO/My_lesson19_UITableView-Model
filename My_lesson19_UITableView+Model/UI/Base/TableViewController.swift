d882483c6ac87ead5efc30adc757613d6e2583e9
Oleg Chudnovskiy MB_Pro_13 <oleg.chudnovskiy@gmail.com>
2020-05-11 12:50:43 +0300

    TableViewController fish, 0:47:56

--- My_lesson19_UITableView+Model/UI/Base/TableViewController.swift
+++ My_lesson19_UITableView+Model/UI/Base/TableViewController.swift
@@ -11,14 +11,48 @@ import UIKit
 class TableViewController: ViewController, UITableViewDataSource, UITableViewDelegate {
 
     var dataSourse = [Any]()
-    var tableView: UITableView?
     
+    @IBOutlet var tableView: UITableView?
+    var tableViewStyle: UITableView.Style {
+        return .plain
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupTableView()
     }
     
+    //MARK: -Setup TableView
+    private func setupTableView() {
+        if self.tableView == nil {
+            let tableView = UITableView(frame: self.view.bounds, style: tableViewStyle)
+            self.view.addSubview(tableView)
+            
+            tableView.translatesAutoresizingMaskIntoConstraints = false
+            let constraints = [
+                tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
+                tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+                tableView.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 0),
+                tableView.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: 0),
+            ]
+            NSLayoutConstraint.activate(constraints)
+            
+            self.tableView = tableView
+        }
+        
+        self.tableView?.dataSource = self
+        self.tableView?.delegate = self
+        //self.tableView?.separatorStyle = .none
+        self.tableView?.backgroundColor = .clear
+        
+        registerCells()
+    }
+    
+    func registerCells() {
+         
+    }
+    
+    //MARK: -UITableViewDataSource
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -31,5 +65,7 @@ class TableViewController: ViewController, UITableViewDataSource, UITableViewDel
         assert(true, "Need override this method  in SubClass")
         return UITableViewCell()
     }
+
+    //MARK: -UITableViewDelegate
 
 }
